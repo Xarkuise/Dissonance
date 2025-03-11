2f14b1b59b2300a4b4195a50cd3c7b93e3307a59
Xarkuise <adriansatentes@gmail.com>
2025-03-12 04:48:18 +0800

    Update

---
+++ Assets/Scripts/Player Controller/PlayerController.cs
@@ -0,0 +1,174 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("References")]
+
+    public Rigidbody rb;
+    public Transform head;
+    public Camera camera;
+
+    [Header("Camera Effects")]
+    public float baseCameraHeight = .85f;
+    public float walkBobbingRate = .75f;
+    public float runBobbingRate = 1f;
+    public float maxWalkBobbingOffset = .2f;
+    public float maxRunBobbingOffset = .3f;
+
+
+    [Header("Configurations")]
+    public float walkSpeed;
+    public float runSpeed;
+    public float jumpSpeed;
+    public float itemPickupDistance;
+
+    [Header("Runtime")]
+    Vector3 newVelocity;
+    bool isGrounded = false;
+    bool isJumping = false;
+    Transform attachedObject = null;
+    float attachedDistance = 0f;
+
+    [Header("Audio")]
+    public AudioSource audioWalk;
+
+    void Start() {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        //Horizontal rotation
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 2f);
+
+        //Run Speed and Walk Speed
+        newVelocity = Vector3.up * rb.velocity.y;
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        newVelocity.x = Input.GetAxis("Horizontal") * speed;
+        newVelocity.z = Input.GetAxis("Vertical") * speed;
+
+        //Jump
+        if (isGrounded){
+            if (Input.GetKeyDown(KeyCode.Space) && !isJumping){
+                newVelocity.y = jumpSpeed;
+                isJumping = true;
+            }
+        }
+
+        bool isMovingOnGround = (Input.GetAxis("Vertical") != 0f || Input.GetAxis("Horizontal") != 0f) && isGrounded;
+
+        if(isMovingOnGround)
+        {
+            float bobbingRate = Input.GetKey(KeyCode.LeftShift) ? runBobbingRate : walkBobbingRate;
+            float bobbingOffset = Input.GetKey(KeyCode.LeftShift) ? maxRunBobbingOffset : maxWalkBobbingOffset;
+            Vector3 targetHeadPosition = Vector3.up * baseCameraHeight + Vector3.up * (Mathf.PingPong(Time.time * bobbingRate, bobbingOffset) - bobbingOffset * .5f);
+            head.localPosition = Vector3.Lerp(head.localPosition, targetHeadPosition, .1f);
+        }
+
+        rb.velocity = transform.TransformDirection(newVelocity);   //Follow the head(mouse) rotation movement
+
+        //Audio
+        if (isMovingOnGround)
+        {
+            if (!audioWalk.isPlaying) audioWalk.Play(); // Play only if not already playing
+        }
+        else
+        {
+            audioWalk.Stop(); // Stop the audio when not moving
+        }
+        audioWalk.pitch = Input.GetKey(KeyCode.LeftShift) ? 1.75f : 1f;
+
+        // Picking Items
+        RaycastHit hit;
+        bool cast = Physics.Raycast(head.position, head.forward, out hit, itemPickupDistance);
+
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (attachedObject !=null)
+            {
+                attachedObject.SetParent(null);
+
+                if (attachedObject.GetComponent<Rigidbody>() != null)
+                    attachedObject.GetComponent<Rigidbody>().isKinematic = false;
+
+                if (attachedObject.GetComponent<Collider>() != null)
+                    attachedObject.GetComponent<Collider>().enabled = true;
+
+                attachedObject = null;
+            }
+            else
+            {
+                if (cast)
+                {
+                    if (hit.transform.CompareTag("pickable"))
+                    {
+                        attachedObject = hit.transform;
+                        attachedObject.SetParent(transform);
+
+                        if (attachedObject.GetComponent<Rigidbody>() != null)
+                            attachedObject.GetComponent<Rigidbody>().isKinematic = true;
+
+                        if (attachedObject.GetComponent<Collider>() != null)
+                            attachedObject.GetComponent<Collider>().enabled = false;
+                    }
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate(){
+       
+      
+
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1f))
+            isGrounded = true;
+        else isGrounded = false;
+
+    }
+
+    private void LateUpdate(){
+        //Vertical Rotation
+        Vector3 e = head.eulerAngles;
+        e.x -= Input.GetAxis("Mouse Y") * 2f;
+        e.x = RestrictAngle(e.x, -85f, 85f);
+        head.eulerAngles = e;
+
+        if(attachedObject != null)
+        {
+            attachedObject.position = head.position + head.forward * attachedDistance;
+            attachedObject.Rotate(transform.right * Input.mouseScrollDelta.y * 30f, Space.World);
+        }
+    }
+
+    public static float RestrictAngle(float angle, float angleMin, float angleMax){
+        //Angle of the Player
+        if (angle > 180)
+            angle -= 360;
+        else if (angle < -180)
+            angle += 360;
+
+        if (angle > angleMax)
+            angle = angleMax;
+        if (angle < angleMin)
+            angle = angleMin;
+
+        return angle;
+    }
+
+
+    void OnCollisionStay(Collision col){
+        isGrounded = true;
+        isJumping = false;
+    }
+
+    void OnCollisionExit(Collision col){
+        isGrounded = false;
+    }
+}
